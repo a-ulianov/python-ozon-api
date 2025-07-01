f3a10ffaba604b0cc7e0a4d70554de06a38dd774
Mephisto Fox <jakyll.fox@gmail.com>
2025-07-01 14:32:24 +0300

    feat: Добавление файла setup.py для настройки пакета и описание класса OzonProductArchiveAPI с методами архивирования и разархивирования товаров через Ozon API

--- ozon_api/methods/product_archive.py
+++ ozon_api/methods/product_archive.py
@@ -9,9 +9,24 @@ from ozon_api.models.product_archive import (
 
 
 class OzonProductArchiveAPI(OzonAPIBase):
+    """
+    Класс для работы с архивированием и разархивированием товаров через Ozon API.
+
+    Предоставляет методы для архивирования и восстановления товаров с помощью соответствующих эндпоинтов Ozon API.
+    """
     async def product_archive(
         self: Type["OzonProductArchiveAPI"], request: ProductArchiveRequest
     ) -> ProductArchiveResponse:
+        """
+        Архивирует товары на платформе Ozon.
+
+        Отправляет POST-запрос к эндпоинту 'product/archive' для архивирования указанных товаров.
+
+        :param request: Объект запроса с параметрами для архивирования товаров.
+        :type request: ProductArchiveRequest
+        :return: Ответ API с результатом архивирования товаров.
+        :rtype: ProductArchiveResponse
+        """
         data = await self._request(
             method="post",
             api_version="v1",
@@ -23,6 +38,16 @@ class OzonProductArchiveAPI(OzonAPIBase):
     async def product_unarchive(
         self: Type["OzonProductArchiveAPI"], request: ProductUnarchiveRequest
     ) -> ProductUnarchiveResponse:
+        """
+        Разархивирует товары на платформе Ozon.
+
+        Отправляет POST-запрос к эндпоинту 'product/unarchive' для восстановления ранее архивированных товаров.
+
+        :param request: Объект запроса с параметрами для разархивирования товаров.
+        :type request: ProductUnarchiveRequest
+        :return: Ответ API с результатом разархивирования товаров.
+        :rtype: ProductUnarchiveResponse
+        """
         data = await self._request(
             method="post",
             api_version="v1",
